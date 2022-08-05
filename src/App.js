4ec47bfc9354a6346b36df169842b75d1929da0c
seanliaus001 <106371370+seanliaus001@users.noreply.github.com>
2022-08-05 19:41:24 +1000

    Update App.js (#18)
    add name list under title.

--- src/App.js
+++ src/App.js
@@ -42,6 +42,9 @@ function App({ signOut }) {
     <Flex direction={"column"}>
       <Flex justifyContent={'space-between'}>
         <Heading level={1}>My notes app!</Heading>
+        <ol>
+          <li>XIANGLI</li>
+        </ol>
         <Button onClick={signOut}>Sign Out</Button>
       </Flex>
       {notes.map(note => <Flex alignItems={'center'}>
